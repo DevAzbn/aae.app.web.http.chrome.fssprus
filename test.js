d0b952d91ca8c45d328eb32395dba7b303faab36
azbn <alexander.zybin@yandex.ru>
2017-12-07 20:45:03 +0300

    201712072045

--- test.js
+++ test.js
@@ -14,10 +14,12 @@ azbn.setMdl('config', require('./config/main'));
 
 azbn.setMdl('url', require('url'));
 
+/*
 var Iconv = require('iconv').Iconv;
 var fromEnc = 'cp1251';
 var toEnc = 'utf8';
 var translator = new Iconv(fromEnc, toEnc);
+*/
 
 	//http://fssprus.ru/torgi/action/city?data=5700000000000
 	//http://fssprus.ru/torgi/ajax_search/?torgi[bidnumber]=&torgi[status]=5&torgi[torgpublishdate][from]=&torgi[torgpublishdate][to]=&torgi[propname]=&torgi[region]=5700000000000&torgi[city]=&torgi[startprice][from]=&torgi[startprice][to]=&torgi[torgexpiredate][from]=&torgi[torgexpiredate][to]=
@@ -157,13 +159,14 @@ azbn.mdl('web/http', {
 
 
 
-const r2 = require('r2');
-let html = await r2.get(
+const needle = require('needle');
+
+needle.get(
 	'http://fssprus.ru/torgi/ajax_search/?torgi[bidnumber]=&torgi[status]=5&torgi[torgpublishdate][from]=&torgi[torgpublishdate][to]=&torgi[propname]=&torgi[region]=5700000000000&torgi[city]=&torgi[startprice][from]=&torgi[startprice][to]=&torgi[torgexpiredate][from]=&torgi[torgexpiredate][to]=',
 	{
 		headers : {
 			'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 YaBrowser/17.10.1.1204 Yowser/2.5 Safari/537.36',
-			'Accept' : '*/*',
+			'Accept' : '* /*',
 			'Accept-Encoding' : 'gzip, deflate',
 			'Accept-Language' : 'ru,en;q=0.8',
 			'Cache-Control' : 'no-cache',
@@ -173,11 +176,90 @@ let html = await r2.get(
 			'Pragma' : 'no-cache',
 			'Referer' : 'http://fssprus.ru/torgi/',
 			'X-Requested-With' : 'XMLHttpRequest',
-			//'encoding' : null,
+			'encoding' : null,
 			//'proxy' : 'http://localproxy.com',
 			//'rejectUnauthorized' : false,
 		},
-	}
-).text;
+	}, function(error, response, body) {
+	
+		if(error) {
+			
+			console.log(error);
+			
+		} else {
+			//console.log(body);
+			
+			var table_selector = '.results-frame table';
+			
+			//
+			var $ = azbn.mdl('web/http').parse(body);
+			
+			var table = $(table_selector);
+			
+			var runTask_result = [];
+			
+			if(table.length > 0) {
+				
+				var trs = table.find('tr');
+				
+				trs.each(function(index){
+					
+					var tr = $(this);
+					
+					var
+						notificationId = 0,
+						lotId = 0,
+						url = null
+					;
+					
+					var as = tr.find('td:nth-child(2) a');
+					as.each(function(index){
+						var a = $(this);
+						var href = a.attr('href');
+						href = href + '';
+						href = href.replace(/&amp;/ig, '&');
+						url = azbn.mdl('url').parse(href, true, true);
+						notificationId = url.query.notificationId;
+						lotId = url.query.lotId;
+					});
+					
+					var title = tr.find('td:nth-child(3)').html();
+					
+					var sum = tr.find('td:nth-child(4)').html();
+					sum = sum + '';
+					sum = sum.split('.');
+					sum = sum[0];
+					sum = sum.replace(/\s/ig, '');
+					if(sum != '' && sum.length > 0) {
+						sum = parseInt(sum);
+					} else {
+						sum = 0;
+					}
+					
+					if(sum > 0) {
+						runTask_result.push({
+							notificationId : notificationId,
+							lotId : lotId,
+							title : title,
+							sum : sum,
+						});
+					}
+					
+				});
+				
+			}
+			
+			if(runTask_result.length) {
+				for(var i = 0; i < runTask_result.length; i++) {
+					
+					var item = runTask_result[i];
+					app.mkDataDir('notifications/' + item.notificationId);
+					app.saveJSON('notifications/' + item.notificationId + '/' + item.lotId, item);
+					
+				}
+			}
+			console.dir(runTask_result);
+			
+		}
 
-console.log(html);
+});
